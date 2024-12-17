91034c1941ae95a8b801658ebb7bb9a03317b1d5
prolinkpc <prolinkpc2@gmail.com>
2024-12-17 09:25:50 +0530

    Initializing Spring Boot Project.

---
+++ src/main/java/com/example/horsebettingbackend/HorseBettingBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.horsebettingbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HorseBettingBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HorseBettingBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/horsebettingbackend/HorseBettingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.horsebettingbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HorseBettingBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
