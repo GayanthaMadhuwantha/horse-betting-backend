6d6113f6bf3180d23f4cd5b1a63de183097c8101
GayanthaMadhuwantha <134595232+GayanthaMadhuwantha@users.noreply.github.com>
2024-12-17 10:24:00 +0530

    Initializing Spring Boot Project.

--- src/main/java/com/example/horsebettingbackend/HorseBettingBackendApplication.java
+++ src/main/java/com/example/horsebettingbackend/HorseBettingBackendApplication.java
@@ -2,11 +2,14 @@ package com.example.horsebettingbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class HorseBettingBackendApplication {
-
-    @Get
+    @GetMapping("/")
+    public String hello(){
+        return "hello";
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(HorseBettingBackendApplication.class, args);
